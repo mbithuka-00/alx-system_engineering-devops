a4e6e2183249eb6af6d201695a9463bc7b5bc616
mbithuka-00 <mbithukamatthew@gmail.com>
2023-11-24 03:37:23 -0800

    my commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# site.pp
+
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+}
+
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+this is my readme file for configurationmanagement task 
+here we look at puppet and how it is the best in devops
